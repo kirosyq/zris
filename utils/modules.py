ca7e1d8d086b070ecbeaa3244b62d272abba79c6
hodlmod.eth <zasara1980@gmail.com>
2023-10-27 11:00:59 +0500

    fix sleeping

--- utils/modules.py
+++ utils/modules.py
@@ -900,8 +900,8 @@ async def process_batches(func, wallets):
 
         if (TG_BOT_SEND and len(list_send) > 0):
             send_msg()
-            
-        if IS_SLEEP and any(res):
+
+        if IS_SLEEP:
             await async_sleeping(*DELAY_SLEEP)
 
         list_send.clear()
